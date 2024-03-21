ed9e28218225b0d53407197c0aefa711f0e5b672
Gabyx708 <matiascoria427@gmail.com>
2024-03-21 15:54:00 -0300

    inicio del proyecto

---
+++ src/components/Header.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const HeaderComponent: React.FC = (): JSX.Element => {
+    return (
+        <>
+            <header>
+                <div className="logo">
+                    <img src="" alt="logo_image" />
+                </div>
+
+                <div>
+                    inicio
+                </div>
+            </header>
+        </>
+    );
+}
+
+export default HeaderComponent;
